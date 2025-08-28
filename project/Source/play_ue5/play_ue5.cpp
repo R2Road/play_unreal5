5a15484fe9b7db2d8e12a62a2e8a7cde7a7b7648
R2Road <studior2road@gmail.com>
2025-08-28 11:03:09 +0900

    update ue5 version : 5.5 > 5.6

--- project/Source/play_ue5/play_ue5.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "play_ue5.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, play_ue5, "play_ue5" );
